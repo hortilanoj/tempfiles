b516174c05ae1ed30bc3e70c6c6e98d950badf8a
hortilanoj <hortilanoj@gmail.com>
2023-04-21 21:17:05 +0800

    update script

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,2 @@
-./xmrig -o gulf.moneroocean.stream:10128 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 -p wolfminer1
+cd /home/wolfbyte/xmrig/build
+./xmrig -o gulf.moneroocean.stream:10128 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 -p wolflaptop
