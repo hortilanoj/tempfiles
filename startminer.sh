c83c27d58bbfeb590318ac9a8ca82d89758b6e9e
hortilanoj <hortilanoj@gmail.com>
2023-05-05 05:24:40 +0800

    Update startminer.sh script

--- startminer.sh
+++ startminer.sh
@@ -1,3 +1,2 @@
 cd /home/wolfbyte/xmrig/build
-./xmrig -o xmr-asia1.nanopool.org:14444 -a rx/0 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1.wolfminer1 -p hortilanoj@gmail.com
-
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 --tls --coin monero
