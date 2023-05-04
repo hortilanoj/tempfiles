46742d160cd785e1a819084a3124188cabf3defa
hortilanoj <hortilanoj@gmail.com>
2023-05-05 05:36:18 +0800

    Update startminer.sh script

--- startminer.sh
+++ startminer.sh
@@ -1,2 +1,2 @@
 cd /home/wolfbyte/xmrig/build
-./xmrig -o xmr-asia1.nanopool.org:14433 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 --tls --coin monero
+xmrig --url pool.hashvault.pro:7777 --user 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 --pass wolfminer --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
