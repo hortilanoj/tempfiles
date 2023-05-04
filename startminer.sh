6f5213d66e2e4200d27430a9b96c6920d2c066c3
hortilanoj <hortilanoj@gmail.com>
2023-05-04 16:28:52 +0800

    Update startminer,sh commands

--- startminer.sh
+++ startminer.sh
@@ -1,2 +1,3 @@
 cd /home/wolfbyte/xmrig/build
-./xmrig -o gulf.moneroocean.stream:10128 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 -p wolflaptop
+./xmrig -o xmr-asia1.nanopool.org:14444 -u 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1.wolfminer1 -p hortilanoj@gmail.com
+
