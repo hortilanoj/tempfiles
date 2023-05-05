95a4c66340bc6355a5a2d84c9a96fd28dfb9b395
hortilanoj <hortilanoj@gmail.com>
2023-05-05 14:23:30 +0800

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,2 +1,2 @@
 cd /home/wolfbyte/xmrig/build
-./xmrig --url pool.hashvault.pro:7777 --user 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 --pass wolfminer --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./xmrig --url pool.hashvault.pro:5555 --user 491G7wEvdkvLy4QNkBGuerVAjhRMcfEo3TTN7SgnJx68R7KnAvD2XxZUMcTvusMCB1RtoYkWsDsNPS9Aa171PcMTEJpSTR1 --pass wolfminer --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
